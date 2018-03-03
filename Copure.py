98b2829ffb7e7062b4f8a92bc3706358ba840122
Vishnu R <vishnurapps@gmail.com>
2018-03-03 22:26:39 +0530

    Added Menubar, central widget and status bar

--- Copure.py
+++ Copure.py
@@ -1,38 +1,21 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import  *
 
-class Copure(QWidget):
+class Copure(QMainWindow):
     def __init__(self, parent=None):
         super(Copure, self).__init__(parent)
 
-        self.newButton = QPushButton("New")
-        self.loadButton = QPushButton("Load")
-        self.cropButton = QPushButton("Crop")
-        self.readButton = QPushButton("Read")
-        self.sendButton = QPushButton("Send")
-        self.quitButton = QPushButton("Quit")
+        menubar = self.menuBar()
+        centralwidget = QWidget()
+        self.createAction(menubar)
+        self.createButtons(centralwidget)
+        self.statusBar()
+        self.setCentralWidget(centralwidget)
 
-        self.frontPageLayout = QHBoxLayout()
-        self.frontPageLayout.addWidget(self.newButton)
-        self.frontPageLayout.addWidget(self.loadButton)
-        self.frontPageLayout.addWidget(self.cropButton)
-        self.frontPageLayout.addWidget(self.readButton)
-        self.frontPageLayout.addWidget(self.sendButton)
-        self.frontPageLayout.addWidget(self.quitButton)
-
-        self.mainLayout = QHBoxLayout()
-        # mainLayout.addWidget(nameLabel, 0, 0)
-        self.mainLayout.addLayout(self.frontPageLayout)
-
-        self.setLayout(self.mainLayout)
         self.setWindowTitle("Copure")
-
-        self.newButton.clicked.connect(self.doNew)
-        self.loadButton.clicked.connect(self.doLoad)
-        self.cropButton.clicked.connect(self.doCrop)
-        self.readButton.clicked.connect(self.doRead)
-        self.sendButton.clicked.connect(self.doSend)
-        self.quitButton.clicked.connect(self.close)
+        self.setWindowIcon(QIcon('cut.png'))
+        self.statusBar().showMessage('Ready')
 
     def doNew(self):
         print("Inside doNew")
@@ -48,6 +31,56 @@ class Copure(QWidget):
 
     def doSend(self):
         print("Inside doSend")
+
+    def createAction(self, menubar):
+        exitAct = QAction('&Exit', self)
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        timeOutAct = QAction('&Timeout', self)
+        timeOutAct.setStatusTip('Set screenshot timeout')
+        timeOutAct.triggered.connect(qApp.quit)
+
+        emailAct = QAction('&Email Configuration', self)
+        emailAct.setStatusTip('Configure the emails')
+        emailAct.triggered.connect(qApp.quit)
+
+        aboutCopureAct = QAction('&About', self)
+        aboutCopureAct.setStatusTip('About Copure')
+        aboutCopureAct.triggered.connect(qApp.quit)
+
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+        editMenu = menubar.addMenu('&Edit')
+        editMenu.addAction(timeOutAct)
+        editMenu.addAction(emailAct)
+        aboutMenu = menubar.addMenu('&About')
+        aboutMenu.addAction(aboutCopureAct)
+
+    def createButtons(self, centralWidget):
+        newButton = QPushButton("New", self)
+        loadButton = QPushButton("Load", self)
+        cropButton = QPushButton("Crop", self)
+        readButton = QPushButton("Read", self)
+        sendButton = QPushButton("Send", self)
+        quitButton = QPushButton("Quit", self)
+
+        frontPageLayout = QHBoxLayout()
+        frontPageLayout.addWidget(newButton)
+        frontPageLayout.addWidget(loadButton)
+        frontPageLayout.addWidget(cropButton)
+        frontPageLayout.addWidget(readButton)
+        frontPageLayout.addWidget(sendButton)
+        frontPageLayout.addWidget(quitButton)
+
+        newButton.clicked.connect(self.doNew)
+        loadButton.clicked.connect(self.doLoad)
+        cropButton.clicked.connect(self.doCrop)
+        readButton.clicked.connect(self.doRead)
+        sendButton.clicked.connect(self.doSend)
+        quitButton.clicked.connect(self.close)
+
+        centralWidget.setLayout(frontPageLayout)
 
 if __name__ == '__main__':
     import sys
