c2a32760f17e37d51eaf1741c5b60b7a09477386
Vishnu R <vishnurapps@gmail.com>
2018-03-04 13:49:50 +0530

    Added ability to take new screenshot

--- Copure.py
+++ Copure.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import  *
+import time
 
 class Copure(QMainWindow):
     def __init__(self, parent=None):
@@ -17,8 +18,36 @@ class Copure(QMainWindow):
         self.setWindowIcon(QIcon('cut.png'))
         self.statusBar().showMessage('Ready')
 
+    def test(self):
+        print("TIme over")
+
+    def tick(self):
+        print('tick')
+
     def doNew(self):
         print("Inside doNew")
+        self.hide()
+        time.sleep(5)
+        if(self.isHidden()):
+
+            self.timerScreen = QTimer()
+            self.timerScreen.setInterval(2000)
+            self.timerScreen.setSingleShot(True)
+            self.timerScreen.timeout.connect(self.tick)
+
+            QApplication.instance().beep()
+            screen = QApplication.primaryScreen()
+            if screen is not None:
+                self.originalPixmap = screen.grabWindow(0)
+            else:
+                self.originalPixmap = QPixmap()
+            print(self.screenshotLabel.width())
+            print(self.screenshotLabel.height())
+            self.screenshotLabel.setPixmap(self.originalPixmap.scaled(
+                self.screenshotLabel.size(), Qt.KeepAspectRatio,
+                    Qt.SmoothTransformation))
+            self.screenshotLabel.show()
+        self.show()
 
     def doLoad(self):
         print("Inside doLoad")
@@ -58,6 +87,16 @@ class Copure(QMainWindow):
         aboutMenu.addAction(aboutCopureAct)
 
     def createButtons(self, centralWidget):
+
+        self.screenshotLabel = QLabel()
+        self.screenshotLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.screenshotLabel.setAlignment(Qt.AlignCenter)
+        screen_resolution = app.desktop().screenGeometry()
+        width, height = screen_resolution.width(), screen_resolution.height()
+        padding = 150
+        self.screenshotLabel.setMinimumSize(width-padding,height-padding)
+        self.screenshotLabel.hide()
+
         newButton = QPushButton("New", self)
         loadButton = QPushButton("Load", self)
         cropButton = QPushButton("Crop", self)
@@ -80,7 +119,11 @@ class Copure(QMainWindow):
         sendButton.clicked.connect(self.doSend)
         quitButton.clicked.connect(self.close)
 
-        centralWidget.setLayout(frontPageLayout)
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.screenshotLabel)
+        mainLayout.addLayout(frontPageLayout)
+
+        centralWidget.setLayout(mainLayout)
 
 if __name__ == '__main__':
     import sys
