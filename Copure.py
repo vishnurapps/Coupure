ac62b62dd566123ac883701daefcb31cb8fc7415
vLal-v <vinethlal.v@gmail.com>
2018-03-10 20:11:28 +0530

    Added UI for Email configuration

--- Copure.py
+++ Copure.py
@@ -1,7 +1,96 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import  *
+import xml.etree.cElementTree as ET
 import time
+import re
+
+class EmailConfigPopUp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI() 
+
+    def initUI(self):
+        self.setWindowTitle("Email configuation")
+
+        lblUserName = QLabel("User Name", self)
+        lblHostName = QLabel("Host", self)
+        lblPort = QLabel("Port", self)
+
+
+        # TODO: layout adjustments
+        hlayout0 = QHBoxLayout()
+        self.tbHostAddr = QLineEdit()
+
+        onlyInt = QIntValidator()
+        self.tbPort = QLineEdit()
+        self.tbPort.setValidator(onlyInt)
+
+        hlayout0.addWidget(lblHostName)
+        hlayout0.addWidget(self.tbHostAddr)
+        hlayout0.addWidget(lblPort)
+        hlayout0.addWidget(self.tbPort)
+
+        self.tbUserName = QLineEdit()
+        hlayout1 = QHBoxLayout()
+        hlayout1.addWidget(lblUserName)
+        hlayout1.addWidget(self.tbUserName)
+
+        okButton = QPushButton("OK")
+        okButton.clicked.connect(self.saveEmailConfig)
+        cancelButton = QPushButton("Cancel")    
+        cancelButton.clicked.connect(self.closePopUp)   
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        vlayout = QVBoxLayout()
+        vlayout.addLayout(hlayout1)        
+        vlayout.addLayout(hlayout0)
+        vlayout.addLayout(vbox)
+
+        popUpLayout = QVBoxLayout()        
+        popUpLayout.addLayout(vlayout)
+
+        self.setLayout(popUpLayout)
+        self.setFixedSize(400, 100)
+        self.readEmailConfig()
+        self.show()
+    def saveEmailConfig(self):
+        if(not self.validateEmailConfig()):
+            #TODO: messagebox invalid input
+            return False
+        self.writeEmailConfig()
+        self.close()
+    def closePopUp(self):
+        self.close()
+    def readEmailConfig(self):        
+        try:
+            e = ET.parse('EmailConfig.xml').getroot()
+            self.tbUserName.setText(e.find("EmailConfig").find('UserName').text)
+            self.tbHostAddr.setText(e.find("EmailConfig").find("Host").text)
+            self.tbPort.setText(e.find("EmailConfig").find("Port").text)
+        except:
+            self.tbUserName.setText('')
+            self.tbHostAddr.setText('')
+            self.tbPort.setText('')
+    def validateEmailConfig(self):
+        return re.match(r"[^@]+@[^@]+\.[^@]+", self.tbUserName.text())
+    def writeEmailConfig(self):        
+        root = ET.Element("root")
+        doc = ET.SubElement(root, "EmailConfig")
+
+        ET.SubElement(doc, "UserName").text = self.tbUserName.text()
+        ET.SubElement(doc, "Host").text = self.tbHostAddr.text()
+        ET.SubElement(doc, "Port").text = self.tbPort.text()
+
+        tree = ET.ElementTree(root)
+        tree.write("EmailConfig.xml")
+        return True
 
 class Copure(QMainWindow):
     def __init__(self, parent=None):
@@ -61,6 +150,11 @@ class Copure(QMainWindow):
     def doSend(self):
         print("Inside doSend")
 
+    def emailConfig(self):
+        self.w = EmailConfigPopUp()
+        #self.w.setGeometry(QRect(100, 100, 400, 100))
+        self.w.show()
+
     def createAction(self, menubar):
         exitAct = QAction('&Exit', self)
         exitAct.setStatusTip('Exit application')
@@ -72,7 +166,7 @@ class Copure(QMainWindow):
 
         emailAct = QAction('&Email Configuration', self)
         emailAct.setStatusTip('Configure the emails')
-        emailAct.triggered.connect(qApp.quit)
+        emailAct.triggered.connect(self.emailConfig)
 
         aboutCopureAct = QAction('&About', self)
         aboutCopureAct.setStatusTip('About Copure')
